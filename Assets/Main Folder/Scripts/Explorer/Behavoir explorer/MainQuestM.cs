5dac8cc5a26842a33376c8cdacf73f02bf1ddeef
Rafael Expósito Pérez <r.exposito.2018@gmail.com>
2021-12-09 23:55:53 +0100

    kjgfh

--- Assets/Main Folder/Scripts/Explorer/Behavoir explorer/MainQuestM.cs
+++ Assets/Main Folder/Scripts/Explorer/Behavoir explorer/MainQuestM.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Timeline;
+
 using UnityEngine;
-using UnityEngine.AI;
 
 public class MainQuestM : MonoBehaviour
 {
